66dec9bf3bc1fa25181ea3261376e7abb95689f7
pedrogallietta <pedro.rrg2@gmail.com>
2021-02-01 14:53:11 -0300

    Criação projeto trabalhador

---
+++ rh-trabalhador/src/test/java/com/gallietta/rhtrabalhador/RhTrabalhadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallietta.rhtrabalhador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTrabalhadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
