de3a3ce0b20df50fa3585046f2d9f709969cb084
pedrogallietta <pedro.rrg2@gmail.com>
2021-02-02 15:26:17 -0300

    Hystrix timout

--- rh-folha-pagamento/src/main/java/com/gallietta/rhfolhapagamento/controller/PagamentoController.java
+++ rh-folha-pagamento/src/main/java/com/gallietta/rhfolhapagamento/controller/PagamentoController.java
@@ -1,5 +1,7 @@
 package com.gallietta.rhfolhapagamento.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gallietta.rhfolhapagamento.entidades.Pagamento;
 import com.gallietta.rhfolhapagamento.negocio.PagamentoService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/pagamentos")
@@ -17,11 +20,15 @@ public class PagamentoController {
     @Autowired
     private PagamentoService service;
     
+    @HystrixCommand(fallbackMethod = "getPagamentoAlternativo")
     @GetMapping(value = "/{trabalhadorId}/dias/{dias}")
     public ResponseEntity<Pagamento> getPagamento(@PathVariable Long trabalhadorId, @PathVariable Integer dias) {
         Pagamento pagamento = service.getPagamento(trabalhadorId, dias);
-        
         return ResponseEntity.ok(pagamento);
+    }
+    
+    public ResponseEntity<Pagamento> getPagamentoAlternativo(Long trabalhadorId, Integer dias) {
+        return ResponseEntity.ok(new Pagamento("RH Trabalhador fora do ar", BigDecimal.ZERO, 0));
     }
     
 }
