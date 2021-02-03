5077ffe5204c478fd66c929e0be7c174a2e37e1a
pedrogallietta <pedro.rrg2@gmail.com>
2021-02-03 11:29:26 -0300

    rh-trabalhador como cliente config server

--- rh-trabalhador/src/main/java/com/gallietta/rhtrabalhador/controller/TrabalhadorController.java
+++ rh-trabalhador/src/main/java/com/gallietta/rhtrabalhador/controller/TrabalhadorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,20 @@ public class TrabalhadorController {
     
     private static Logger      logger = LoggerFactory.getLogger(TrabalhadorController.class);
     
+    @Value("${test.config}")
+    private String             testConfig;
+    
     @Autowired
     private Environment        env;
     
     @Autowired
     private TrabalhadorService service;
+    
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfig() {
+        logger.info("Config = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
     
     @GetMapping
     public ResponseEntity<List<Trabalhador>> buscaTodos() {
