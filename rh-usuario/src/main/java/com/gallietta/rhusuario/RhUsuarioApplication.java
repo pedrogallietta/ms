d0432b5a50d7790d5c4046bff73a08ae6d42093b
pedrogallietta <pedro.rrg2@gmail.com>
2021-02-03 14:44:04 -0300

    Criação do projeto rh usuario

---
+++ rh-usuario/src/main/java/com/gallietta/rhusuario/RhUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.gallietta.rhusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhUsuarioApplication.class, args);
+	}
+
+}
---
+++ rh-usuario/src/test/java/com/gallietta/rhusuario/RhUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallietta.rhusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
