dccf0f307fb751840e89fe30e925418065e1832f
pedrogallietta <pedro.rrg2@gmail.com>
2021-02-03 15:12:05 -0300

    Populando base de dados

--- rh-usuario/src/main/java/com/gallietta/rhusuario/RhUsuarioApplication.java
+++ rh-usuario/src/main/java/com/gallietta/rhusuario/RhUsuarioApplication.java
@@ -1,15 +1,26 @@
 package com.gallietta.rhusuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class RhUsuarioApplication {
+public class RhUsuarioApplication implements CommandLineRunner {
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(RhUsuarioApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        // System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+    }
+    
 }
